218ebf1ee49723df369ac2af837f8c2bce6182a0
David Allen <dallen@zendesk.com>
2017-09-14 11:27:58 +1000

    Get app running

--- dist/assets/app.js
+++ dist/assets/app.js
@@ -22459,6 +22459,10 @@ var _react = __webpack_require__(49);
 
 var _react2 = _interopRequireDefault(_react);
 
+var _AjaxWrapper = __webpack_require__(187);
+
+var _AjaxWrapper2 = _interopRequireDefault(_AjaxWrapper);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
